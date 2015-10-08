c112841ab43e8cbef45ec751c70f3eadb00d190d
justiny117 <justiny117@gmail.com>
2015-10-08 07:29:24 +0000

    new2

--- app/controllers/learn_controller.rb
+++ app/controllers/learn_controller.rb
@@ -1,12 +1,7 @@
 class LearnController < ApplicationController
   def index
     @chat = Chat.all
-    Chat.create(chatinput: "1", chatoutput: "1")
-    Chat.create(chatinput: "2", chatoutput: "2")
-    Chat.create(chatinput: "3", chatoutput: "3")
-    Chat.create(chatinput: "a", chatoutput: "1")
-    Chat.create(chatinput: "a", chatoutput: "2")
-    Chat.create(chatinput: "a", chatoutput: "3")
+
   end
   def create
     Chat.create(chatinput: params[:ask], chatoutput: params[:answer])
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+    Chat.create(chatinput: "1", chatoutput: "1")
+    Chat.create(chatinput: "2", chatoutput: "2")
+    Chat.create(chatinput: "3", chatoutput: "3")
+    Chat.create(chatinput: "a", chatoutput: "1")
+    Chat.create(chatinput: "a", chatoutput: "2")
+    Chat.create(chatinput: "a", chatoutput: "3")
+    puts "OK"
